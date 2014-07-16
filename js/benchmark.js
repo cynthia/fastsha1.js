d63f824f434715a93922b47cd6598bd39b0d0a7e
Sangwhan Moon <sangwhan@iki.fi>
2014-07-17 01:58:15 +0900

    Fixed indentation and added Rusha and SCJL for benchmarks.

--- js/benchmark.js
+++ js/benchmark.js
@@ -2,22 +2,26 @@ var mRuns = 1;
 var mUseHighRes = false;
 var mTimeSum = 0;
 var mTime;
+var mConversionTimeSum;
+var mConversionTime;
 
 var kFastSha1       = 0;
 var kOriginalSha1   = 1;
 var kMtSha1         = 2;
 var kCryptoJSSha1   = 3;
 var kJsSha1         = 4;
+var kScjlSha1       = 5;
+var kRusha          = 6;
 
-var kTags = ['fast', 'orig', 'mt', 'cryptojs', 'jssha'];
+var kTags = ['fast', 'orig', 'mt', 'cryptojs', 'jssha', 'scjl', 'rusha'];
 
 var kFirstType      = kFastSha1;
 var kLastType       = kJsSha1;
 
-var kAscii			= 0;
-var kKorean			= 1;
-var kJapanese		= 2;
-var kMixed			= 3;
+var kAscii          = 0;
+var kKorean         = 1;
+var kJapanese       = 2;
+var kMixed          = 3;
 
 function startTimer() {
     mTime = mUseHighRes ? performance.now() : Date.now();
@@ -36,13 +40,23 @@ function jsSha1Wrapper(input) {
     return shaObj.getHash("SHA-1", "HEX");
 }
 
+function scjlSha1Wrapper(input) {
+    var hashArray = sjcl.hash.sha1.hash(input);
+    return toHex(hashArray[0]) + toHex(hashArray[1]) + toHex(hashArray[2]) + toHex(hashArray[3]) + toHex(hashArray[4]);
+}
+
+function rushaWrapper(input) {
+    var r = new Rusha();
+    return r.digest(toUTF8(input));
+}
+
 function benchmark(input, type) {
     mTimeSum = 0;
     var tag;
 
-	tag = kTags[type];
+    tag = kTags[type];
 
-	switch (type)
+    switch (type)
     {
         case kFastSha1:
             fn = FastSHA1;
@@ -58,6 +72,12 @@ function benchmark(input, type) {
             break;
         case kJsSha1:
             fn = jsSha1Wrapper;
+            break;
+        case kScjlSha1:
+            fn = scjlSha1Wrapper;
+            break;
+        case kRusha:
+            fn = rushaWrapper;
             break;
         default:
             alert('ASSERT: Function should not reach here.');
@@ -80,40 +100,75 @@ function updateRunCount(count) {
 }
 
 function generateTestData(type) {
-	var string;
+    var string;
 
-	switch (type)
-	{
-		case kAscii:
-			string = 'The quick brown fox jumped over the lazy dogs. ';
-			break;
-		case kKorean:
-			string = '빠른 여우가 게으른 개들 위를 뛰어 넘었다. ';
-			break;
-		// FIXME: This is nonsense from Google Translate.
-		case kJapanese:
-			string = '速いキツネが怠惰な犬の上を越えた。 '
-			break;
-		case kMixed:
-			string = 'The はやい brown フォックス 점프 over the lazy 犬. ';
-			break;
-		default:
-			alert('ASSERT: Function should not reach here.');
+    switch (type)
+    {
+        case kAscii:
+            string = 'The quick brown fox jumped over the lazy dogs. ';
+            break;
+        case kKorean:
+            string = '빠른 여우가 게으른 개들 위를 뛰어 넘었다. ';
+            break;
+        // FIXME: This is nonsense from Google Translate.
+        case kJapanese:
+            string = '速いキツネが怠惰な犬の上を越えた。 '
+            break;
+        case kMixed:
+            string = 'The はやい brown フォックス 점프 over the lazy 犬. ';
+            break;
+        default:
+            alert('ASSERT: Function should not reach here.');
             return;
-	}
+    }
 
-	var buf = '';
+    var buf = '';
 
-	for (var i = 0; i <= 4096; i++)
-	{
-	    buf += string;
-	}
+    for (var i = 0; i <= 4096; i++)
+    {
+        buf += string;
+    }
 
-	document.getElementById('in').value = buf;
+    document.getElementById('in').value = buf;
 }
 
+function toHex(val) {
+    var str = "";
+    var i;
+    var v;
+
+    for (i = 7; i >= 0; i--) {
+        v = (val >>> (i * 4)) & 0x0f;
+        str += v.toString(16);
+    }
+    return str;
+};
+
+// Rusha does not have a UTF8 converter, so copying it from fastsha.js
+function toUTF8(string) {
+    var utftext = "";
+
+    for (var n = 0; n < string.length; n++) {
+        var c = string.charCodeAt(n);
+
+        if (c < 128) {
+            utftext += String.fromCharCode(c);
+        }
+        else if (c > 2047) {
+            utftext += String.fromCharCode((c >> 12) | 224);
+            utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+            utftext += String.fromCharCode((c & 63) | 128);
+        }
+        else {
+            utftext += String.fromCharCode((c >> 6) | 192);
+            utftext += String.fromCharCode((c & 63) | 128);
+        }
+    }
+    return utftext;
+};
+
 document.addEventListener('DOMContentLoaded', function(e) {
-	mUseHighRes = typeof window.performance === 'undefined' ? false : true;
-	generateTestData(kMixed);
-	updateRunCount(document.getElementById('runcount').value);
+    mUseHighRes = typeof window.performance === 'undefined' ? false : true;
+    generateTestData(kMixed);
+    updateRunCount(document.getElementById('runcount').value);
 }, false);
